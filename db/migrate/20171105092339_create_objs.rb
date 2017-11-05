d7a26d2666229fe27cc9fa24f34011b6aa76f614
adg3748 <revivedtomorrow@gmail.com>
2017-11-05 18:29:31 +0900

    add migration

---
+++ db/migrate/20171105092339_create_objs.rb
@@ -0,0 +1,11 @@
+class CreateObjs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :objs do |t|
+      t.string :title
+      t.integer :influence
+      t.integer :time
+      t.deadline :deadline
+      t.timestamps
+    end
+  end
+end
