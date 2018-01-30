82327ae2c6bb041e6ec100efa9166d04b754d2aa
adg3748 <revivedtomorrow@gmail.com>
2018-01-30 11:55:26 +0900

    タスク名フロントでの編集を可能に

--- app/assets/javascripts/objs.js
+++ app/assets/javascripts/objs.js
@@ -1,1 +1,21 @@
-alert('hello');
+$(document).on('turbolinks:load', function(){
+  console.log($('span[id="title"]'));
+  var n = 1;
+  $('span[id="title"]').each(function(){
+    console.log(this);
+    $(this).click(function(){
+      if(!$(this).hasClass('on')){
+        $(this).addClass('on');
+        var txt = $(this).text();
+        $(this).html('<input type="text" value="'+txt+'" />');
+        $('#title > input').focus().blur(function(){
+          var inputVal = $(this).val();
+          if(inputVal === ''){
+            inputVal = this.defaultValue;
+          }
+          $(this).parent().removeClass('on').text(inputVal);
+        });
+      }
+    });
+  });
+});
