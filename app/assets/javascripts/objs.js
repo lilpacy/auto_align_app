af876b53c4c463e0744bf55fecfbd71ea6548f66
adg3748 <revivedtomorrow@gmail.com>
2018-02-15 12:54:16 +0900

    レイアウトの崩れを修正

--- app/assets/javascripts/objs.js
+++ app/assets/javascripts/objs.js
@@ -169,10 +169,10 @@ $(document).on('turbolinks:load', function(){
     var deadline = obj.deadline ? ''+time.year+'/'+time.month+'/'+time.date+'' : '　　　　　';
     $('div.li-wrapper').append(
       '<li class="row">'+
-        '<span id="title" class="col-md-6" data="'+obj.id+'">'+obj.title+'</span>'+
+        '<span id="title" class="col-md-5" data="'+obj.id+'">'+obj.title+'</span>'+
         '<span id="influence" class="col-md-2 col-xs-3" data="'+obj.id+'">'+obj.influence+'</span>'+
         '<span id="time" class="col-md-2 col-xs-3" data="'+obj.id+'">'+obj.time+'</span>'+
-        '<span id="deadline" class="col-md-2 col-xs-6" data="'+obj.id+'">'+
+        '<span id="deadline" class="col-md-3 col-xs-6" data="'+obj.id+'">'+
           '<p class="deadline">'+deadline+'</p>'+
           '<p class="delete" data="'+obj.id+'">削除</p>'+
         '</span>'+
